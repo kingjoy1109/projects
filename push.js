a40f7890a27bdf9869395cbfbff7fb8bea07d2dd
King Adam <kingjoy1109@gmail.com>
2020-11-04 12:05:43 +0800

    fix

--- push.js
+++ push.js
@@ -13,15 +13,6 @@ webpush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let sub = {
-  endpoint:
-    "https://sg2p.notify.windows.com/w/?token=BQYAAADpwf8CbQYC8daWYc9sQAxNO44Dsea4UbrhkqH25sodxIdcaK0a4XESS5VWPIrk0v1Vn5j5qrDHwWut6eI9bqHgeNOf2UnTD8HQZwZK3G3zbWxqhFYp8P3rJM4DOvEPK3pgaQ8BJeN3y50H87jmA6bO%2bQy8%2fNomapQPp2dq7ne1JO3e4blJSI82xtkeMP9o1xdbSLKZZPBHh07cY%2fSQm5Tjt%2fJq0wySiRMteNw9wSqTGllCoyd8DFgExTt2b6rCOyLuQWg0E%2fMrWibBDVhkgr4lYuOeZYvUAhRF%2btZrPhxD6o%2bd%2b9BiD5ie27WjpHHXZ9Zo1nqiPNn4Aarvlfp3QXLp",
-  expirationTime: null,
-  keys: {
-    p256dh:
-      "BH-02Qzt-ay8aXXpMdH1yMmNlMcx7O9L5atRTG9zY8CvanICLSm28NZYGr70AHlqfe0qAH5FcQKcQm5gLKUsduA",
-    auth: "DYrx44uWBcYWgaSP21DMsA",
-  },
-};
+let sub = code;
 
-webpush.sendNotification(sub, "Test Message");
+// webpush.sendNotification(sub, "Test Message");
