4f40c04334002a0a7f66b8ac9b0d37fbbb9ceb2b
King Adam <kingjoy1109@gmail.com>
2020-11-04 10:14:25 +0800

    initial

---
+++ push.js
@@ -0,0 +1,18 @@
+var webpush = require('web-push');
+
+// const vapidKeys = webpush.generateVAPIDKeys();
+const vapidKeys = {
+    publicKey: 'BAwt0Q-xmEsMJmiUogjK0-8jifPsB2NAR_5eSATzHLd3LYA6Jm_diDN643cigQQl-_kkApym-Mb5815qf2EUN5w',
+    privateKey: 'hq6-moPrJx_qQBEuSxgV9g--EAzLwXgHF_UzyJ8Id60'
+}
+
+webpush.setVapidDetails(
+    'mailto:test@code.co.uk',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+  );
+
+
+  let sub= {};
+
+  push.sendNotification(sub, 'Test Message')
---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('psuh', ()=>{
+    self.registration.sendNotification("Test Message",{
+        
+    })
+})
